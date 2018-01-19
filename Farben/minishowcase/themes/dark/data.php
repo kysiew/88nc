10952b0ab0b8fbe8e5b07c57be4d0ff3f16f4bca
kysiew <35565199+kysiew@users.noreply.github.com>
2018-01-19 06:23:55 +0000

    Add files via upload

---
+++ Farben/minishowcase/themes/dark/data.php
@@ -0,0 +1,54 @@
+<?php
+	/**** default theme ****/
+	
+	$theme_id = "default";
+	
+	//// images
+	$theme_image_alert = "alert.gif";
+	$theme_image_next = "next.gif";
+	$theme_image_prev = "prev.gif";
+	$theme_image_loader = "spinner.gif";
+	
+	//// font families
+	$theme_fonts_titles = "'Trebuchet MS', Tahoma, Verdana, Geneva, Arial";
+	$theme_fonts_text = "Verdana, Geneva, Arial";
+	
+	//// colors
+	$theme_line_color = "#333";
+	
+	$theme_general_text = "#FFF";
+	$theme_general_text_light = "#999";
+	$theme_general_dimmed = "#75b86e";
+	$theme_general_bg = "#33463d";
+	
+	$theme_menu_bg = "#486958";
+	$theme_menu_text = "#75b86e";
+	$theme_menu_small = "#FFF";
+	$theme_menu_link = "#FFF";
+	$theme_menu_active = "#75b86e";
+	$theme_menu_hover = "#8d8d92";
+	$theme_menu_bg_hover = "#CCC";
+	
+	$theme_thumbnail_normal = "#8d8d92";
+	$theme_thumbnail_selected = "#8d8d92";
+	$theme_thumbnail_hover = "#FFF";
+	
+	$theme_lightbox_bg = "#FFF";
+	$theme_lightbox_title = "#FFF";
+	$theme_lightbox_text = "#666";
+	$theme_lightbox_link = "#999";
+	$theme_lightbox_hover = "#999";
+	$theme_lightbox_line = "#FFF";
+	$theme_lightbox_selected = "#FFF";
+	
+	$theme_lightbox_color = "#33463d";
+	$theme_lightbox_opacity = "80";
+	
+	$theme_alert_bg = "#F30";
+	$theme_alert_text = "#FFF";
+	$theme_alert_link = "#F90";
+	$theme_alert_hover = "#FFF";
+	
+	$theme_footer_text = "#FFF";
+	$theme_footer_hover = "#666";
+?>
