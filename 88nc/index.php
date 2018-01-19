7d52764a3fb302df8564259eb35f4c602f03b466
kysiew <35565199+kysiew@users.noreply.github.com>
2018-01-19 03:55:45 +0000

    Add files via upload

--- 88nc/index.php
+++ 88nc/index.php
@@ -1,1 +1,160 @@
+<?php
+$root_path = './';
+include($root_path . 'header.php');
+?>
+        <table width="100%" border="0" bordercolor="#afaeae" cellspacing="0" cellpadding="0" align="center">
+          <tr>
+            <td align="center" valign="center" bgcolor="#ffffff" width="550"><br>
+              <table width="522" border="0" bordercolor="#afaeae" cellspacing="0" cellpadding="0" align="center">
+                <tr>
+	              <td bgcolor="#ffffff" width="35">&nbsp;</td>
+	              <td bgcolor="#ffffff" width="13" height="33" background="images/greenleft.gif">&nbsp;</td>
+	              <td align="center" valign="center" width="180" background="images/greencenter.gif" class="gen">BEGINNER WEB HOSTING</td>
+	              <td bgcolor="#ffffff" width="13" background="images/greenright.gif">&nbsp;</td>
+	              <td bgcolor="#ffffff" width="30">&nbsp;</td>
+	              <td bgcolor="#ffffff" width="13" background="images/greenleft.gif">&nbsp;</td>
+	              <td align="center" valign="center" width="180" background="images/greencenter.gif" class="gen">BASIC WEB DESIGN</td>
+	              <td bgcolor="#ffffff" width="13" background="images/greenright.gif">&nbsp;</td>
+	              <td bgcolor="#ffffff" width="35">&nbsp;</td>
+                </tr>
+                <tr>
+	              <td bgcolor="#ffffff">&nbsp;</td>
+	              <td bgcolor="#ffffff" background="images/gleft.gif">&nbsp;</td>
+	              <td align="left" valign="top" background="images/HP-Ser1.gif"><br>
+	              <div class="gen"><img src="images/hot.gif"><font color="#fd7f19">RM100/year</font></div>
+	              <div class="genmed">
+	              <li>1GB Disk Space</li>
+	              <li>POP3/SMTP Account Supported</li>
+	              <li>MySQL Database Supported</li>
+	              <li>Perl/CGI, PHP</li>
+	              <li><b>No</b> Setup Fee</li>
+	              </div><br>
+	              <div align="center"><input class="genbutton" type="button" value="SIGN UP" onclick="window.location='message.php';"></div>
+	              </td>
+	              <td bgcolor="#ffffff" background="images/gright.gif">&nbsp;</td>
+	              <td bgcolor="#ffffff">&nbsp;</td>
+	              <td bgcolor="#ffffff" background="images/gleft.gif">&nbsp;</td>
+	              <td align="left" valign="top" background="images/design.gif"><br>
+	              <div class="gen"><img src="images/hot.gif"><font color="#fd7f19">RM450</font></div>
+	              <div class="genmed">
+	              <li>Landing Page</li>
+	              <li>4 Static Pages</li>
+	              <li>Enquiry/Feedback Form</li>
+	              <li>Web Counter &amp; Statistics</li>
+	              <li><b>Free</b> 3 Times Content Update</li>
+	              </div><br>
+	              <div align="center"><input class="genbutton" type="button" value="SIGN UP" onclick="window.location='message.php';"></div>
+	              </td>
+	              <td bgcolor="#ffffff" background="images/gright.gif">&nbsp;</td>
+	              <td bgcolor="#ffffff">&nbsp;</td>
+                </tr>
+                <tr>
+	              <td valign="bottom" bgcolor="#ffffff" height="16">&nbsp;</td>
+	              <td valign="bottom" bgcolor="#ffffff" background="images/gbleft.gif">&nbsp;</td>
+	              <td valign="bottom" bgcolor="#ffffff" background="images/gbcenter.gif"></td>
+	              <td valign="bottom" bgcolor="#ffffff" background="images/gbright.gif">&nbsp;</td>
+	              <td valign="bottom" bgcolor="#ffffff">&nbsp;</td>
+	              <td valign="bottom" bgcolor="#ffffff" background="images/gbleft.gif">&nbsp;</td>
+	              <td valign="bottom" bgcolor="#ffffff" background="images/gbcenter.gif"></td>
+	              <td valign="bottom" bgcolor="#ffffff" background="images/gbright.gif">&nbsp;</td>
+	              <td valign="bottom" bgcolor="#ffffff">&nbsp;</td>
+                </tr>
+              </table><br>
+            </td>
+            <td rowspan="3" align="left" valign="bottom" bgcolor="#ffffff" width="3" background="images/dot.gif">
+            </td>
+            <td rowspan="3" align="left" valign="top" bgcolor="#ffffff" width="247" background="images/side.gif">
+              <table cellspacing="0" cellpadding="2" width="100%">
+          		<!--<tr>
+              	  <td bgcolor="#918a8a" class="xsmall" height="20" align="center"><b>Domain Name Lookup</b></td>
+          		</tr>
+          		<tr>
+              	  <td class="genmed" valign="top">
+	              	  Enter a domain name to check availability: <br>
+	              	  www.<input type="text" size="20">.
+	              	  <select name="">
+	              	  <option name="">.com</option>
+	              	  <option name="">.net</option>
+	              	  <option name="">.info</option>
+	              	  <option name="">.org</option>
+	              	  <option name="">.biz</option>
+	              	  <option name="">.hk</option>
+	              	  <option name="">.tw</option>
+	              	  <option name="">.us</option>
+	              	  <option name="">.my</option>
+	              	  <option name="">.com.my</option>
+	              	  </select><br><br>
+	              	  </td>
+          		</tr>
+          		<tr>
+              	  <td class="genmed" valign="top" align="center">
+              	  <input class="genbutton" type="button" value="SEARCH"><br><br>
+              	  </td>
+          		</tr>-->
+          		<tr>
+              	  <td bgcolor="#918a8a" class="xsmall" height="20" align="center"><b>Testimonials </b></td>
+          		</tr>
+          		<tr>
+              	  <td class="genmed" valign="top">
+              	  	"Your invaluable help in designing and developing the website engine project is deeply appreciated. It proved that we have obtained the best service and value, so very cost-effective.  Your creative ideas gave our business an immense impact on our corporate image. Without a second thought, I would recommend your company to anyone who requires practical IT solutions." <br><br>
+					<b>Tony Wong<br>
+					<a href="http://www.bgrw.com" target="new">BGRW Trading</a></b><br><br>
+              	  </td>
+          		</tr>
+          		<tr>
+              	  <td bgcolor="#918a8a" class="xsmall" height="20" align="center"><b>Promotions</b></td>
+          		</tr>
+          		<tr>
+              	  <td class="genmed" valign="top">
+              	  	<a href="freeWebsite.php"><b>Sign up</b></a> now for <b><font color="red">FREE</font></b> Company Website!<img src="images/hot.gif"><br>
+              	  	Absolutely free! No hidden cost!<br><br>
+              	  </td>
+          		</tr>
+          		<tr>
+              	  <td bgcolor="#918a8a" class="xsmall" height="20" align="center"><b>Live Chat</b></td>
+          		</tr>
+          		<tr>
+              	  <td class="genmed" valign="top">
+              	  	We can be contacted via <br>
+              	  	<script language="JavaScript" type="text/javascript"><!--
+	if ( navigator.userAgent.toLowerCase().indexOf('mozilla') != -1 && navigator.userAgent.indexOf('5.') == -1 && navigator.userAgent.indexOf('6.') == -1 )
+		document.write(' <a href="http://wwp.icq.com/scripts/search.dll?to=40236195"></a>');
+	else
+		document.write('<div style="position:relative"><div style="position:absolute"><a href="http://wwp.icq.com/scripts/search.dll?to=40236195"></a></div><div style="position:absolute;left:3px;top:-1px"><a href="http://wwp.icq.com/40236195#pager"><img src="http://web.icq.com/whitepages/online?icq=40236195&img=5" width="18" height="18" border="0" /></a></div></div>');
 
+				//--></script><noscript><a href="http://wwp.icq.com/scripts/search.dll?to=40236195"></a></noscript>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;ICQ#: 40236195<br><br>
+				  	<a href="ymsgr:SendIM?cyinseong"><img border=0 src="http://opi.yahoo.com/online?u=cyinseong&m=g&t=2">
+</a>&nbsp;&nbsp;<br>YAHOO ID: cyinseong<br><br>
+					<img border=0 src="images/msn1.gif">&nbsp;&nbsp;MSN ID: seong22@hotmail.com
+              	  </td>
+          		</tr>
+              </table>
+            </td>
+          </tr>
+          <tr>
+            <td align="left" valign="center" bgcolor="#4261a1" width="550" class="xsmall" height="35" background="images/test.gif">
+            <font color="#ffffff">&nbsp;<b>:: Our Technology Partners ::</b></font>
+            </td>
+          <tr>
+            <td align="left" valign="bottom" bgcolor="#ffffff" width="550">
+            <applet codebase="./" code="imgScroller.class" width="550" height="85">
+			   <param name="img" value="./images/partner.gif">
+			   <param name="bgcolor" value="#FFFFFF">
+			   <param name="bordercolor" value="#ffffff">
+			   <param name="border" value="true">
+			   <param name="delay" value="25">
+			   <param name="startx" value="30">
+			   <param name="starty" value="0">
+			   <param name="stepx" value="1">
+			   <param name="stepy" value="10">
+			   <param name="scrollingstop" value="true">
+			   <param name="imagedrag" value="true">
+			   <param name="target" value="_self">
+			   <param name="linkcount" value="2">
+			</applet>
+            </td>
+          </tr>
+        </table>
+<?php
+include($root_path . 'footer.html');
+?>        
